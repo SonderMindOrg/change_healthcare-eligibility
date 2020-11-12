a25da9303949948ad16ff68c0e9f474cc00a4798
Anthony Super <asuper@sondermind.com>
2020-11-12 15:09:49 -0700

    Initial

---
+++ lib/change_healthcare/eligibility.rb
@@ -0,0 +1,8 @@
+require "change_healthcare/eligibility/version"
+
+module ChangeHealthcare
+  module Eligibility
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/change_healthcare/eligibility_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe ChangeHealthcare::Eligibility do
+  it "has a version number" do
+    expect(ChangeHealthcare::Eligibility::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
